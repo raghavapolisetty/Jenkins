d09cb39551d981e63579f0d4e76881b97d5315d5
Admin <Admin@Madala>
2020-12-11 18:00:47 +0530

    final push

--- WorkonJenkins/src/main/java/jenkins/AddMethods.java
+++ WorkonJenkins/src/main/java/jenkins/AddMethods.java
@@ -2,21 +2,36 @@ package jenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class AddMethods {
-
+	static WebDriver driver;	
 	
-	static WebDriver driver;
-	public static void main(String[] args) {
-
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\seleniumsession\\drivers\\chromedriver.exe");
+	@BeforeTest
+	public void Launch()
+			{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\git\\Jenkins\\WorkonJenkins\\drivers\\chromedriver.exe");
 		
 		driver = new ChromeDriver();
+		
+			}
+	
+	@Test
+	public void Opensite(){
+		
 		driver.manage().window().maximize();
 		driver.get("https://www.flipkart.com");
 		
 		System.out.println("opened");
 		System.out.println(driver.getTitle());
+	
+	}
+	
+	@AfterTest
+	public void teardown() {
+		driver.close();
 	}
 
 }
